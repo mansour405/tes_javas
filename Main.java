e0e8b9405c1572efb661c83acbfb4887312d4794
Mansour <ngommansour100@gmail.com>
2024-06-08 12:08:16 +0000

    function add done

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main (String[] args){
-        System.out.println("Hello Mansour");
+        System.out.println(x:"Hello Mansour");
+        System.out.println(x:"Calling add :");
+        add(x:4, y:8);
+    }
+    public static void add(int x, int y){
+        System.out.println(x+" + "+y+" = "+(x+y));
     }
 }
